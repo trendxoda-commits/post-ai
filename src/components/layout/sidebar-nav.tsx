ad20891453d590c5d38ca150c7563e73fdd7f66d
TrendXoda <trendxoda@gmail.com>
2025-10-27 03:49:00 +0000

    Error: `legacyBehavior` is deprecated and will be removed in a future re

--- src/components/layout/sidebar-nav.tsx
+++ src/components/layout/sidebar-nav.tsx
@@ -23,19 +23,17 @@ export function SidebarNav() {
     <SidebarMenu>
       {navItems.map((item) => (
         <SidebarMenuItem key={item.href}>
-          <Link href={item.href} passHref legacyBehavior>
-            <SidebarMenuButton
-              asChild
-              isActive={pathname === item.href}
-              tooltip={item.label}
-              className="justify-start"
-            >
-              <a>
-                <item.icon className="h-4 w-4" />
-                <span>{item.label}</span>
-              </a>
-            </SidebarMenuButton>
-          </Link>
+          <SidebarMenuButton
+            asChild
+            isActive={pathname === item.href}
+            tooltip={item.label}
+            className="justify-start"
+          >
+            <Link href={item.href}>
+              <item.icon className="h-4 w-4" />
+              <span>{item.label}</span>
+            </Link>
+          </SidebarMenuButton>
         </SidebarMenuItem>
       ))}
     </SidebarMenu>

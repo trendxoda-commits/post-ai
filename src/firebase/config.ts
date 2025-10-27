9a62a16a0bb1f5797130d0fd4b41162fce2ef892
TrendXoda <trendxoda@gmail.com>
2025-10-27 03:55:32 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3392506130-ee5cc",
+  "appId": "1:37004993461:web:92029b88864a3b984deffc",
+  "apiKey": "AIzaSyA-pajNkynnnQi-wD3Ymx9w3dQ5erws8pI",
+  "authDomain": "studio-3392506130-ee5cc.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "37004993461"
+};

9b4a59e7739684b9407b25abd2ff78901510ca78
TrendXoda <trendxoda@gmail.com>
2025-10-27 07:38:48 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -35,6 +35,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: '**.cdninstagram.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };

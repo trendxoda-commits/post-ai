0a8cb5ec4f33df574844683d26621b7aab031ed9
TrendXoda <trendxoda@gmail.com>
2025-11-01 13:08:32 +0000

    mhi hata h abhi ayrr abhi  bahi mere

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  devIndicators: {
-    buildActivity: false,
-  },
+  devIndicators: false,
   typescript: {
     ignoreBuildErrors: true,
   },
